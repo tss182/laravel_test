915758a805ff7f6c4ef5af03c6735c6d4324250c
tss182 <suryatriyana@gmail.com>
2024-05-21 20:50:03 +0700

    webstore

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 import 'flowbite';
+
+let navbar = document.querySelector("#navbar")
+
+// Navbar Fixed
+window.onscroll = function () {
+    const fixedNav = navbar.offsetTop;
+
+    if (window.scrollY > fixedNav) {
+        navbar.classList.add('navbar-fixed');
+    } else {
+        navbar.classList.remove('navbar-fixed');
+    }
+};
--- resources/views/components/layout-nav.blade.php
+++ resources/views/components/layout-nav.blade.php
@@ -1,23 +1,25 @@
 <x-layout>
-    <div class="w-full">
+    <div id="navbar" class="w-full">
         <nav class="bg-pink-200 py-5">
             <div class="flex items-center justify-between">
                 <div class="flex items-center space-x-1 md:space-x-0 rtl:space-x-reverse">
-                    <button  data-drawer-target="sidebar"  data-drawer-toggle="sidebar" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700" aria-controls="sidebar" aria-expanded="false">
+                    <button  data-drawer-target="sidebar"  data-drawer-toggle="sidebar" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-pink-500 rounded-lg md:hidden hover:bg-pink-100" aria-controls="sidebar" aria-expanded="false">
                         <span class="sr-only">Open main menu</span>
-                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-                        </svg>
+                        <x-ei-navicon class="size-12"/>
                     </button>
                 </div>
-                <a href="https://flowbite.com/" class="flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                <a href="/" class="flex items-center">
+                    <img src="{{asset('/images/logo.png')}}" class="h-5" alt="Logo" />
                 </a>
-                <div class="flex items-center">
-                    <button type="button" data-dropdown-toggle="language-dropdown-menu" class="inline-flex items-center font-medium justify-center px-4 py-2 text-sm text-gray-900 dark:text-white rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
-                        <svg class="w-5 h-5 rounded-full me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 3900 3900"><path fill="#b22234" d="M0 0h7410v3900H0z"/><path d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0" stroke="#fff" stroke-width="300"/><path fill="#3c3b6e" d="M0 0h2964v2100H0z"/><g fill="#fff"><g id="d"><g id="c"><g id="e"><g id="b"><path id="a" d="M247 90l70.534 217.082-184.66-134.164h228.253L176.466 307.082z"/><use xlink:href="#a" y="420"/><use xlink:href="#a" y="840"/><use xlink:href="#a" y="1260"/></g><use xlink:href="#a" y="1680"/></g><use xlink:href="#b" x="247" y="210"/></g><use xlink:href="#c" x="494"/></g><use xlink:href="#d" x="988"/><use xlink:href="#c" x="1976"/><use xlink:href="#e" x="2470"/></g></svg>
-                        English (US)
+                <div class="flex items-center mr-5">
+                    <button class="text-white font-extrabold">
+                        <x-ei-search class="size-10"/>
+                    </button>
+                    <button class="text-white font-extrabold">
+                        <x-ei-user class="size-10"/>
+                    </button>
+                    <button class="text-white font-extrabold">
+                        <x-ei-cart class="size-10"/>
                     </button>
                     <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700" id="language-dropdown-menu">
                         <ul class="py-2 font-medium" role="none">
@@ -82,7 +84,7 @@
         <div class="h-full px-3 py-4 overflow-y-auto bg-pink-50">
             <ul class="space-y-2 font-medium list-none">
                 @foreach($menus as $m)
-                    <li class="focus:border-none">
+                    <li>
                         <a href="{{ $m['link'] }}" class="nav-link group">
                             @if($m['icon'] != '')
                             <x-dynamic-component :component="$m['icon']" class="size-8" />
